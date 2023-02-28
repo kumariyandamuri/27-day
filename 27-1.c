608d91662665a06e432c4d6c93ebd98faf92c59d
kumariyandamuri <125617730+kumariyandamuri@users.noreply.github.com>
2023-02-28 06:46:05 +0530

    Add files via upload

---
+++ 27-1.c
@@ -0,0 +1,34 @@
+/*#include<stdio.h>
+main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+		printf("%d",j);
+		
+	
+		}
+		printf("\n");
+		
+   }
+	
+}*/
+#include<stdio.h>
+main()
+{
+	int i,n;
+	for(n=1;n<=5;n++)
+	{
+		for(i=1;i<=n;i++)
+		{
+			printf("%d",i);
+		}
+		
+		
+		printf("\n");
+
+	}
+	
+}
---
+++ 27-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	char i,j;
+	for(i=65;i<=90;i++)
+	
+	{
+		for(j=65;j<=i;j++)
+		{
+		
+		printf("%c",j);
+	
+	}
+		printf("\n");
+}
+}
